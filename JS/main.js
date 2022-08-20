0b78d7ab8164e2c8410d55e72f5f5e2582462fab
Shalvin shaji <shalvinshaji283@gmail.com>
2022-08-20 22:11:25 +0530

    Volume section started working

--- JS/main.js
+++ JS/main.js
@@ -86,5 +86,131 @@ let All_song = [
 ];
 
 
+// All functions
 
 
+// function load the track
+function load_track(index_no){
+	clearInterval(timer);
+	reset_slider();
+
+	track.src = All_song[index_no].path;
+	title.innerHTML = All_song[index_no].name;	
+  artist.innerHTML = All_song[index_no].singer;
+  track.load();
+
+	timer = setInterval(range_slider ,1000);
+}
+
+load_track(index_no);
+
+
+//mute sound function
+function mute_sound(){
+	track.volume = 0;
+  
+}
+// change volume
+function volume_change(){
+	track.volume = recent_volume.value / 100;
+}
+
+
+// checking.. the song is playing or not
+ function justplay(){
+ 	if(Playing_song==false){
+ 		playsong();
+
+ 	}else{
+ 		pausesong();
+ 	}
+ }
+
+
+// reset song slider
+ function reset_slider(){
+ 	slider.value = 0;
+ }
+
+// play song
+function playsong(){
+  track.play();
+  Playing_song = true;
+  play.innerHTML = '<i class="bx bx-pause-circle"></i>';
+}
+
+//pause song
+function pausesong(){
+	track.pause();
+	Playing_song = false;
+	play.innerHTML = '<i class="bx bx-play-circle"></i>';
+}
+
+
+// next song
+function next_song(){
+	if(index_no < All_song.length - 1){
+		index_no += 1;
+		load_track(index_no);
+		playsong();
+	}else{
+		index_no = 0;
+		load_track(index_no);
+		playsong();
+
+	}
+}
+
+
+// previous song
+function previous_song(){
+	if(index_no > 0){
+		index_no -= 1;
+		load_track(index_no);
+		playsong();
+
+	}else{
+		index_no = All_song.length;
+		load_track(index_no);
+		playsong();
+	}
+}
+
+
+
+
+// change slider position 
+function change_duration(){
+	slider_position = track.duration * (slider.value / 100);
+	track.currentTime = slider_position;
+}
+
+
+
+
+function range_slider(){
+	let position = 0;
+        
+        // update slider position
+		if(!isNaN(track.duration)){
+		   position = track.currentTime * (100 / track.duration);
+		   slider.value =  position;
+	      }
+
+       
+       // function will run when the song is over
+       if(track.ended){
+           if(index_no < All_song.length - 1){
+            index_no += 1;
+            load_track(index_no);
+            playsong();
+          }else{
+            index_no = 0;
+            load_track(index_no);
+            playsong();
+        
+          }
+		       
+           }
+	    }
+     
