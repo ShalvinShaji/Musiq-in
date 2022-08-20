8bee757d8c5cebb5c8731aab3fd845d4049b0154
Shalvin shaji <shalvinshaji283@gmail.com>
2022-08-21 01:53:34 +0530

    Media player section completed

--- JS/main.js
+++ JS/main.js
@@ -24,15 +24,7 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 
-
-
-
-
-
-
-
-
-// ----------------------------------------------------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let previous = document.querySelector('#pre');
 let play = document.querySelector('#play');
@@ -43,77 +35,176 @@ let recent_volume= document.querySelector('#volume_c');
 let slider = document.querySelector('#duration_slider');
 
 
-
-
-
 let timer;
-let index_no = 0;
+let index_no;
 let Playing_song = false;
 let muted = false;
 
 
-//create a audio Element
+
+
 let track = document.createElement('audio');
 
 
-//All songs list
-let All_song = [
-   {
-     name: "Arabic kuthu",
-     path: "./Songs/1.mp3",
-     singer: "Anirudh ravichander, Jonita gandhi"
-   },
-   {
-     name: "Kadavule Pole",
-     path: "./Songs/2.mp3",
-     singer: "Karthik"
-   },
-   {
-     name: "Naan Un",
-     path: "Songs/3.mp3",
-     singer: "Arjit singh"
-   },
-   {
-    name: "Kudukku",
-     path: "Songs/4.mp3",
-     singer: "Vineeth sreenivasan"
-   },
-   {
-    name: "Dilbar",
-     path: "Songs/5.mp3",
-     singer: "Neha kakkar"
-   }
+
+
+let Songs = [
+  {
+    name: "Kurumbathi chundari nee",
+    path: "./Songs/Malayalam/m1.mp3",
+    singer: "vineeth sreenivasan"
+  },
+  {
+    name: "Uyiril thodum",
+    path: "./Songs/Malayalam/m2.mp3",
+    singer: "vineeth sreenivasan"
+  },
+  {
+    name: "Mizhyil",
+    path: "./Songs/Malayalam/m3.mp3",
+    singer: "Shahabaz aman"
+  },
+  {
+    name: "Minunundae Mullapolae",
+    path: "./Songs/Malayalam/m4.mp3",
+    singer: "Karthik"
+  },
+  {
+    name: "Lailakame",
+    path: "./Songs/Malayalam/m5.mp3",
+    singer: "Haricharan"
+  }
 
 
 ];
 
-
-// All functions
-
-
-// function load the track
-function load_track(index_no){
-	clearInterval(timer);
+function Startplay(index)
+{
+  var index_n=index;
+  clearInterval(timer);
 	reset_slider();
-
-	track.src = All_song[index_no].path;
-	title.innerHTML = All_song[index_no].name;	
-  artist.innerHTML = All_song[index_no].singer;
+	track.src = Songs[index_n].path;
+	title.innerHTML = Songs[index_n].name;	
+  artist.innerHTML = Songs[index_n].singer;
   track.load();
-
 	timer = setInterval(range_slider ,1000);
+  track.play();
+  Playing_song = true;
+  play.innerHTML = '<i class="bx bx-pause-circle"></i>';
 }
 
-load_track(index_no);
 
 
 
+function justplay(){
+  if(Playing_song==false){
+    playsong();
 
+  }else{
+    pausesong();
+  }
+}
 
+//reset slider
+function reset_slider(){
+  slider.value = 0;
+}
 
+//play song
+function playsong(){
+  track.play();
+  Playing_song = true;
+  play.innerHTML = '<i class="bx bx-pause-circle"></i>';
+ }
+ 
+//pause song
+function pausesong(){
+  track.pause();
+  Playing_song = false;
+  play.innerHTML = '<i class="bx bx-play-circle"></i>';
+ }
+ 
+ 
+ // next song
+ function next_song(){
+  if(Playing_song == true){
+  if(index_no < Songs.length - 1){
+    index_no += 1;
+    Startplay(index_no);
+    playsong();
+  }else{
+    index_no = 0;
+    Startplay(index_no);
+    playsong();
+ 
+  }
+ }
+ else{
+  track.pause();
+ }
+}
+ 
+ 
+ // previous song
+ function previous_song(){
+  if(Playing_song == true){
+  if(index_no > 0){
+    index_no -= 1;
+    Startplay(index_no);
+    playsong();
+ 
+  }else{
+    index_no = Songs.length;
+    Startplay(index_no);
+    playsong();
+  }
+ }
+ else{
+  track.pause();
+ }
+}
 
-
+ 
+ 
+ 
+ 
+ // change slider position 
+ function change_duration(){
+  slider_position = track.duration * (slider.value / 100);
+  track.currentTime = slider_position;
+ }
+ 
+ 
+ 
+ 
+ function range_slider(){
+  let position = 0;
+        
+        // update slider position
+    if(!isNaN(track.duration)){
+       position = track.currentTime * (100 / track.duration);
+       slider.value =  position;
+        }
+ 
+       
+       // function will run when the song is over
+       if(track.ended){
+           if(index_no < Songs.length - 1){
+            index_no += 1;
+            Startplay(index_no);
+            playsong();
+          }else{
+            index_no = 0;
+            Startplay(index_no);
+            playsong();
+        
+          }
+           
+           }
+      }
+     
 //mute sound function
+
 function mute_sound(){
   
   if (muted==false){
@@ -146,124 +237,6 @@ function volume_change(){
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// checking.. the song is playing or not
- function justplay(){
- 	if(Playing_song==false){
- 		playsong();
-
- 	}else{
- 		pausesong();
- 	}
- }
-
-
-// reset song slider
- function reset_slider(){
- 	slider.value = 0;
- }
-
-// play song
-function playsong(){
-  track.play();
-  Playing_song = true;
-  play.innerHTML = '<i class="bx bx-pause-circle"></i>';
-}
-
-//pause song
-function pausesong(){
-	track.pause();
-	Playing_song = false;
-	play.innerHTML = '<i class="bx bx-play-circle"></i>';
-}
-
-
-// next song
-function next_song(){
-	if(index_no < All_song.length - 1){
-		index_no += 1;
-		load_track(index_no);
-		playsong();
-	}else{
-		index_no = 0;
-		load_track(index_no);
-		playsong();
-
-	}
-}
-
-
-// previous song
-function previous_song(){
-	if(index_no > 0){
-		index_no -= 1;
-		load_track(index_no);
-		playsong();
-
-	}else{
-		index_no = All_song.length;
-		load_track(index_no);
-		playsong();
-	}
-}
-
-
-
-
-// change slider position 
-function change_duration(){
-	slider_position = track.duration * (slider.value / 100);
-	track.currentTime = slider_position;
-}
-
-
-
-
-function range_slider(){
-	let position = 0;
-        
-        // update slider position
-		if(!isNaN(track.duration)){
-		   position = track.currentTime * (100 / track.duration);
-		   slider.value =  position;
-	      }
-
-       
-       // function will run when the song is over
-       if(track.ended){
-           if(index_no < All_song.length - 1){
-            index_no += 1;
-            load_track(index_no);
-            playsong();
-          }else{
-            index_no = 0;
-            load_track(index_no);
-            playsong();
-        
-          }
-		       
-           }
-	    }
-     
