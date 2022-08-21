02933fa09f40b5ef35c30a49d7886fae20396908
Shalvin shaji <shalvinshaji283@gmail.com>
2022-08-21 16:23:52 +0530

    Slider created

--- JS/main.js
+++ JS/main.js
@@ -27,13 +27,16 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
+  spaceBetween: 25,
+  
   loop: true,
-  loopFillGroupWithBlank: true,
+  centerSlide:'true',
+  fade:'true',
+  grabCursor:'true',
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    
   },
   navigation: {
     nextEl: ".swiper-button-next",
